81d21c511e348a9abb60f37262ce618a996ced4e
Alex Chick <chick.alexander@gmail.com>
2015-05-10 14:01:45 -0400

    4.2.0.3

--- __DEV__daeious-event-app/__DEV__daeious-event-app/AppDelegate.swift
+++ __DEV__daeious-event-app/__DEV__daeious-event-app/AppDelegate.swift
@@ -6,6 +6,7 @@
 //  Copyright (c) 2015 The Intro Game LLC. All rights reserved.
 //
 
+import Parse
 import UIKit
 
 @UIApplicationMain
@@ -19,6 +20,34 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         -> Bool {
         // Override point for customization after application launch.
         
+            Parse.setApplicationId("AKJFNWcTcG6MUeMt1DAsMxjwU62IJPJ8agbwJZDJ",
+                clientKey: "Zo9ooGl76x6otXYMfErwY6oWihpoLC5Iw4XfIbGS")
+            
+            // Register for Push Notitications
+            if application.applicationState != UIApplicationState.Background {
+                // Track an app open here if we launch with a push, unless
+                // "content_available" was used to trigger a background push (introduced in iOS 7).
+                // In that case, we skip tracking here to avoid double counting the app-open.
+                
+                let preBackgroundPush = !application.respondsToSelector("backgroundRefreshStatus")
+                let oldPushHandlerOnly = !self.respondsToSelector("application:didReceiveRemoteNotification:fetchCompletionHandler:")
+                var pushPayload = false
+                if let options = launchOptions {
+                    pushPayload = options[UIApplicationLaunchOptionsRemoteNotificationKey] != nil
+                }
+                if (preBackgroundPush || oldPushHandlerOnly || pushPayload) {
+                    //PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions) // ** This doesn't work right now...?
+                }
+            }
+            if application.respondsToSelector("registerUserNotificationSettings:") {
+                let userNotificationTypes = UIUserNotificationType.Alert | UIUserNotificationType.Badge | UIUserNotificationType.Sound
+                let settings = UIUserNotificationSettings(forTypes: userNotificationTypes, categories: nil)
+                application.registerUserNotificationSettings(settings)
+                application.registerForRemoteNotifications()
+            } else {
+                let types = UIRemoteNotificationType.Badge | UIRemoteNotificationType.Alert | UIRemoteNotificationType.Sound
+                application.registerForRemoteNotificationTypes(types)
+            }
             
             
             
@@ -91,7 +120,32 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             
         return true
     }
+    
+    // Parse stuff
+    
+    func application(application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData) {
+        let installation = PFInstallation.currentInstallation()
+        installation.setDeviceTokenFromData(deviceToken)
+        //installation.saveInBackground()
+    }
+    
+    func application(application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: NSError) {
+        if error.code == 3010 {
+            println("Push notifications are not supported in the iOS Simulator.")
+        } else {
+            println("application:didFailToRegisterForRemoteNotificationsWithError: %@", error)
+        }
+    }
+    
+    func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject]) {
+        PFPush.handlePush(userInfo)
+        if application.applicationState == UIApplicationState.Inactive {
+            //PFAnalytics.trackAppOpenedWithRemoteNotificationPayload(userInfo)
+        }
+    }
 
+    // end Parse stuff
+    
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
