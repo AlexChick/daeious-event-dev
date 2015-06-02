4fc0cff8d834f7dc1978f20227ba1c66e693f6f2
Alex Chick <chick.alexander@gmail.com>
2015-06-02 13:52:58 -0400

    7.0.2 -- minor modifications, started optimize_event_timing helper function

--- daeious.py
+++ daeious.py
@@ -28,6 +28,56 @@ taste in music? Doing so would require people to connect Daeious with their
 Facebook accounts, which I'm not sure everyone would do. So I guess that's
 something for down the road.
 
+-- Is sec_per_ix something that will change according to how events go?
+Probably. But I should still have some rules -- like, if I want every single
+event to be 1 hour, how are things different when there are 40 ppl vs. 100 ppl?
+Maybe 20m/20w events will just have to be shorter. How much shorter? I can
+figure that out by scaling down how I want a 50m/50w event to be structured.
+    - Given 50m/50w/1mg/1fg,
+        R1 --> sec_per_r1_ix = 15 --> 51 * 15 = 765s = 12.75min = 12m45s.
+               sec_per_r1_ix = 20 --> 51 * 20 = 1020s = 17.00min = 17m00s.
+               sec_per_r1_ix = 25 --> 51 * 25 = 1275s = 21.25min = 21m15s.
+               sec_per_r1_ix = 30 --> 51 * 30 = 1530s = 25.50min = 25m30s.
+
+        R2 --> sec_per_r2_ix = 30 --> 12 * 030 = 360s = 06.00min = 06m 00s.
+               sec_per_r2_ix = 40 --> 12 * 040 = 480s = 08.00min = 08m 00s.
+               sec_per_r2_ix = 45 --> 12 * 045 = 540s = 09.00min = 09m 00s.
+               sec_per_r2_ix = 50 --> 12 * 050 = 600s = 10.00min = 10m 00s.
+               sec_per_r2_ix = 60 --> 12 * 060 = 720s = 12.00min = 12m 00s.
+
+        R3 --> sec_per_r3_ix = 60 --> 05 * 060 = 300s = 05.00min = 05m 00s.
+               sec_per_r3_ix = 80 --> 05 * 080 = 400s = 06.66min = 06m 40s.
+               sec_per_r3_ix = 90 --> 05 x 090 = 450s = 07.50min = 07m 30s.
+               sec_per_r3_ix = 100 -> 05 * 100 = 500s = 08.33min = 08m 20s.
+               sec_per_r3_ix = 120 -> 05 * 120 = 600s = 10.00min = 10m 00s.
+               sec_per_r3_ix = 135 -> 05 * 135 = 675s = 11.25min = 11m 15s.
+               sec_per_r3_ix = 180 -> 05 * 180 = 900s = 15.00min = 15m 00s.
+
+    - Event length (without between-round breaks or pre- or post-game talks):
+        -- Doubling --
+        (15, 30, 60) --> 23m 45s
+        (20, 40, 80) --> 31m 40s
+        (25, 50, 100) -> 39m 35s
+        (30, 60, 120) -> 47m 30s
+        -- Tripling -- 
+        (15, 45, 135) -> 33m 00s
+        (20, 60, 180) -> 44m 00s
+        -- Other --
+        (15, 45, 100)
+        (15, 45, 120)
+        (15, 60, 120)
+        (15, 60, 150)
+        (15, 60, 180)
+        (20, 45, )
+
+
+
+sec_per_ix: 
+    - (Should Round_1 be a constant number of minutes, so that interactions
+        are longer when there are fewer people? Or should the sec_per_ix
+        be a constant for each round? I'll have to play around with that.
+        Right now, I'm leaning towards keeping sec_per_ix constant.)
+
 
 """
 
@@ -117,13 +167,13 @@ class _Event(object):
         self.event_time = random.choice(["19:00", "19:30", "20:00", "20:30"])
         self.event_location = random.choice(["Heaven", "California"])
 
-        self.num_m_eu_p = MEN
-        self.num_f_eu_p = WOMEN
-        self.num_all_eu_p = MEN + WOMEN
-
         self.max_sex = max(MEN,WOMEN)
         self.num_stations = self.max_sex if self.max_sex%2==1 else self.max_sex+1
         self.num_ipads = self.num_stations * 2
+
+        self.num_m_eu_p = MEN
+        self.num_f_eu_p = WOMEN
+        self.num_all_eu_p = MEN + WOMEN
 
         self.num_m_eu_g = self.num_stations - MEN
         self.num_f_eu_g = self.num_stations - WOMEN
@@ -165,12 +215,12 @@ class _Event(object):
         self.li_eu = self.get_event_users()
 
         
-        # self.li_meup = 
-        # self.li_feup = 
-        # self.li_meug = 
-        # self.li_feug = 
-        # self.li_all_meu = self.li_meup + self.li_meug
-        # self.li_all_feu = self.li_feup + self.li_feug
+        # self.li_mpeu = 
+        # self.li_fpeu = 
+        # self.li_mgeu = 
+        # self.li_fgeu = 
+        # self.li_all_meu = self.li_mpeu + self.li_mgeu
+        # self.li_all_feu = self.li_fpeu + self.li_fgeu
         # self.li_all_eu = self.li_all_meu + self.li_all_feu
         # self.li_all_eu.sort(key = lambda x: x.eu_num)
 
@@ -244,12 +294,12 @@ class _Event(object):
     #     """
 
     #     qset_all_users = User.Query.all().order_by("userNum")
-    #     li_meup = list(qset_all_users.filter(sex = "M").limit(self.num_m_eu_p))
-    #     li_feup = list(qset_all_users.filter(sex = "F").limit(self.num_f_eu_p))
-    #     li_meug = list(qset_all_users.filter(sex = "MG").limit(self.num_m_eu_g))
-    #     li_feug = list(qset_all_users.filter(sex = "FG").limit(self.num_f_eu_g))
+    #     li_mpeu = list(qset_all_users.filter(sex = "M").limit(self.num_m_eu_p))
+    #     li_fpeu = list(qset_all_users.filter(sex = "F").limit(self.num_f_eu_p))
+    #     li_mgeu = list(qset_all_users.filter(sex = "MG").limit(self.num_m_eu_g))
+    #     li_fgeu = list(qset_all_users.filter(sex = "FG").limit(self.num_f_eu_g))
 
-    #     li_users_at_event = li_meup + li_feup + li_meug + li_feug
+    #     li_users_at_event = li_mpeu + li_fpeu + li_mgeu + li_fgeu
 
     #     li_eu_obj_to_upload = []
 
@@ -321,21 +371,25 @@ class _Round(object):
         self.num_ix_in_round = self.num_ix_pp * e.num_stations
         self.sec_in_round = self.num_ix_in_round * self.sec_per_ix
 
-
-        """ Do stuff. """
-
-        self.li_ix_in_round = []
-
-        # if self.curr_rd in [1, 2, 3]:
-        #     self.li_ix_in_round = self.create_ix_objects_in_Parse()
-
         pass
 
     def prepare(self):
         pass
 
-    def simulate(self):
-        pass
+    def simulate(self, li = []):
+        """
+        Take a list of interaction objects to simulate.
+        Return the same list, with updated values.
+        """
+
+        if li: # equivalent to, and faster than, if li != []
+            for ix in li:
+                ix.m_see_f = random.randint(0,3) # 0 to 3 inclusive
+                ix.f_see_m = random.randint(0,3)
+                ix.quality = ix.m_see_f + ix.f_see_m
+                ix.same_sac = (ix.m_see_f == ix.f_see_m)
+
+        return li
 
     def analyze(self):
         pass
@@ -389,40 +443,14 @@ class Round_0(_Round):
 
 class Round_1(_Round):
     """
-    --> All men and all women have 1 interaction with each other,
+    All men and all women have 1 interaction with each other,
     moving to their right by 1 iPad / station for each new interaction.
-
-    sec_per_ix: 
-    - Given 50m/50w/1mg/1fg,
-        sec_per_ix = 15 --> Round 1 is 51 * 15 = 765s = 12.75min = 12m45s.
-        sec_per_ix = 20 --> Round 1 is 51 * 20 = 1020 = 17.00min = 17m00s.
-    - (Should Round_1 be a constant number of minutes, so that interactions
-        are longer when there are fewer people? Or should the sec_per_ix
-        be a constant for each round? I'll have to play around with that.
-        Right now, I'm leaning towards keeping sec_per_ix constant.)
-
-    prepare: 
-        - 
-    simulate:
-        - 
-    analyze:
-        - 
     """
 
     def __init__(self, e):
         _Round.__init__(self, e)
         self.round_num = 1
         pass
-
-    def prepare(self):
-        pass
-
-    # def simulate(self):
-    #     pass
-
-    # def analyze(self):
-    #     pass
-
     pass
 
 ###############################################################################
@@ -435,16 +463,6 @@ class Round_2(_Round):
         _Round.__init__(self, e)
         self.round_num = 2
         pass
-
-    def prepare(self):
-        pass
-
-    # def simulate(self):
-    #     pass
-
-    # def analyze(self):
-    #     pass
-
     pass
 
 ###############################################################################
@@ -457,16 +475,6 @@ class Round_3(_Round):
         _Round.__init__(self, e)
         self.round_num = 3
         pass
-
-    def prepare(self):
-        pass
-
-    # def simulate(self):
-    #     pass
-
-    # def analyze(self):
-    #     pass
-
     pass
 
 ###############################################################################
@@ -477,21 +485,10 @@ class Round_4(_Round):
     """
     Postgame stuff.
     """
-
     def __init__(self, e):
         _Round.__init__(self, e)
         self.round_num = 4
         pass
-
-    # def prepare(self):
-    #     pass
-
-    # def simulate(self):
-    #     pass
-
-    # def analyze(self):
-    #     pass
-
     pass
 
 
@@ -501,13 +498,14 @@ class Round_4(_Round):
 
 class _Interaction(object):
 
-    COUNTER = 0
+    CURR_IX_NUM = 0
 
     def __init__(self, e, r, mEU, fEU):
-        _Interaction.COUNTER += 1
+        _Interaction.CURR_IX_NUM += 1
         self.e = e
         self.r = r
-        self.ix_num = _Interaction.COUNTER
+        self.round_num = r.round_num
+        self.ix_num = _Interaction.CURR_IX_NUM
         self.m_eu_num = mEU.eu_num
         self.f_eu_num = fEU.eu_num
         self.simuate()
@@ -524,7 +522,7 @@ class _Interaction(object):
         iters = dict((x,y) for x,y in _Interaction.__dict__.items() 
             if x[:2] != '__' 
                 and x != "simulate"
-                and x != "COUNTER"
+                and x != "CURR_IX_NUM"
             )
         # then update the class items with the instance items
         iters.update(self.__dict__)
@@ -553,8 +551,8 @@ class _User(object):
             self.sex = 'MG'
         elif self.u_num <= 2200:
             self.sex = 'FG'
-        else:
-            raise ValueError("self.u_num {} is out of bounds.".format(self.u_num))
+        # else:
+        #     raise ValueError("self.u_num {} is out of bounds.".format(self.u_num))
         pass
 
     def __iter__(self):
@@ -581,7 +579,8 @@ class _EventUser(_User):
     CURR_EVENTUSER_NUM = 0
 
     def __init__(self):
-        _EventUser.__init__(self)
+        _User.__init__(self)
+        self.u_num -= 2200 # (because I added 2200 users)
         _EventUser.CURR_EVENTUSER_NUM += 1
         self.eu_num = _EventUser.CURR_EVENTUSER_NUM
         pass
@@ -599,27 +598,29 @@ class _EventUser(_User):
 """                                SIMULATE                                 """
 ################################################################################
 
-def simulate(e):
-    # simulates an entire event (all 3 rounds, plus pregame and postgame)
+# def simulate(e):
+#     # simulates an entire event (all 3 rounds, plus pregame and postgame)
 
-    """  Create 5 _Round objects.  """
+#     """  Create 5 _Round objects.  """
 
-    r0 = Round_0(e) if e.start_at_round <= 0 else 0
-    r1 = Round_1(e) if e.start_at_round <= 1 else 0
-    r2 = Round_2(e) if e.start_at_round <= 2 else 0
-    r3 = Round_3(e) if e.start_at_round <= 3 else 0
-    r4 = Round_4(e) if e.start_at_round <= 4 else 0
-    print("\n{} Round objects created.".format(len([r0,r1,r2,r3,r4])))
+#     r0 = Round_0(e) if e.start_at_round <= 0 else 0
+#     r1 = Round_1(e) if e.start_at_round <= 1 else 0
+#     r2 = Round_2(e) if e.start_at_round <= 2 else 0
+#     r3 = Round_3(e) if e.start_at_round <= 3 else 0
+#     r4 = Round_4(e) if e.start_at_round <= 4 else 0
+#     print("\n{} Round objects created.".format(len([r0,r1,r2,r3,r4])))
 
-    """  Simulate the rounds.  """
-    for r in [r0, r1, r2, r3, r4]:
-        if isinstance(r, _Round):
-            print("\nCurrent Round:", r.round_num)
-            r.prepare()
-            r.simulate()
-            r.analyze()
+#     """  Simulate the rounds.  """
+#     for r in [r0, r1, r2, r3, r4]:
+#         if isinstance(r, _Round):
+#             print("\nCurrent Round:", r.round_num)
+#             r.prepare()
+#             r.simulate()
+#             r.analyze()
+
+
    
-    pass
+#     pass
 
 ################################################################################
 ################################################################################
@@ -643,45 +644,120 @@ def main():
     """  Create 1 _Event object.  """
     e = _Event(
         EVENT_NUMBER = 0,
-        MEN = 12,
-        WOMEN = 12,
+        MEN = 50,
+        WOMEN = 50,
         START_AT_ROUND = 0,
         SEC_PER_R1_IX = 20,
         SEC_PER_R2_IX = 40,
         SEC_PER_R3_IX = 60
         )
 
-    """  Simulate event.  """
-    simulate(e)
+    """  Create Users: 1,000 men, 1,000 women, 100 male ghosts, 100 female ghosts.  """
+    _User.CURR_USER_NUM = 0
+    li_mpu = list((_User() for i in range(1000)))
+    li_fpu = list((_User() for i in range(1000)))
+    li_mgu = list((_User() for i in range(100)))
+    li_fgu = list((_User() for i in range(100)))
+    li_all_u = li_mpu + li_fpu + li_mgu + li_fgu
 
-    """  Write all 3 lists of ix objects to Excel. """
+
+    """  Create correct number and type of EventUsers.  """
+    ################
+    ######   I THINK THIS IS WRONG.
+    ######   I should be using li_mpu, li_fpu, etc to get correct eu_nums...shouldn't I?
+    li_mpeu = list((_EventUser() for i in range(e.num_m_eu_p)))
+    li_fpeu = list((_EventUser() for i in range(e.num_f_eu_p)))
+    li_mgeu = list((_EventUser() for i in range(e.num_m_eu_g)))
+    li_fgeu = list((_EventUser() for i in range(e.num_f_eu_g)))
+    li_all_eu = li_mpeu + li_fpeu + li_mgeu + li_fgeu
+
+
+    """  Create up to 5 _Round objects.  """
+    r0 = Round_0(e) if e.start_at_round <= 0 else 0
+    r1 = Round_1(e) if e.start_at_round <= 1 else 0
+    r2 = Round_2(e) if e.start_at_round <= 2 else 0
+    r3 = Round_3(e) if e.start_at_round <= 3 else 0
+    r4 = Round_4(e) if e.start_at_round <= 4 else 0
+    print("\n{} Round objects created.".format(len([r0,r1,r2,r3,r4])))
+
+    """  Simulate the rounds.  """
+    # simulates an entire event (all 3 rounds, plus pregame and postgame)
+
+    li_r1_ix = []
+    li_r2_ix = []
+    li_r3_ix = []
+
+    # li_r1_ix = r1.simulate(li_all_eu)
+    li_r = [r0, r1, r2, r3, r4]
+    li_li_ix_by_r = [0,li_r1_ix, li_r2_ix, li_r3_ix, 0]
+    for r, (index, li) in zip(li_r, enumerate(li_li_ix_by_r)):
+        if isinstance(r, _Round):
+            print("\nCurrent Round:", r.round_num)
+            r.prepare()
+            li = r.simulate(li_all_eu)
+            r.analyze()
+            if index == 1:
+                li_r1_ix = li
+            elif index == 2:
+                li_r2_ix = li
+            else: # index == 3:
+                li_r3_ix = li
+
+
+
+
+
+
+
+
+
+    """  Create all sheets in Excel. """
 
     global WB
-
     WB = xlwt.Workbook()
-
     ws_User = WB.add_sheet('Users', cell_overwrite_ok = True)
-    ws_eu = WB.add_sheet('Event Users', cell_overwrite_ok = True)
+    ws_EventUser = WB.add_sheet('Event Users', cell_overwrite_ok = True)
     ws_r1_ix = WB.add_sheet('R1 Ix', cell_overwrite_ok = True)
     ws_r2_ix = WB.add_sheet('R2 Ix', cell_overwrite_ok = True)
     ws_r3_ix = WB.add_sheet('R3 Ix', cell_overwrite_ok = True)
 
+    #  Users
 
-    """  Create 1,000 men, 1,000 women, 100 male ghosts, 100 female ghosts.  """
-    li_meup = list((_User() for i in range(1000)))
-    li_feup = list((_User() for i in range(1000)))
-    li_meug = list((_User() for i in range(100)))
-    li_feug = list((_User() for i in range(100)))
-    li_all_eu = li_meup + li_feup + li_meug + li_feug
-
-    """  Write the column label names for the Users sheet in Excel  """
-    for c, label in enumerate(dict(li_meup[0]).keys()):
-            ws_User.write(0, c, label)
+    # """  Write the column label names for the Users sheet in Excel  """
+    # for c, label in enumerate(dict(li_mpu[0]).keys()):
+    #         ws_User.write(0, c, label)
 
     """  Write the rows of Users in Excel (too early?)  """
-    for c, label in enumerate(dict(li_all_eu[0]).keys()):
-        for r, user in enumerate(li_all_eu):
+    for c, label in enumerate(dict(li_all_u[0]).keys()):
+        ws_User.write(0, c, label)
+        for r, user in enumerate(li_all_u):
             ws_User.write(r+1, c, getattr(user, label))   
+
+    #  Event Users
+
+    # """  Write the column label names for the EventUsers sheet in Excel  """
+    # for c, label in enumerate(dict(li_mpeu[0]).keys()):
+    #         ws_EventUser.write(0, c, label)
+
+    """  Write the rows of EventUsers in Excel (too early?)  """ # _EventUser
+    for c, label in enumerate(dict(li_all_eu[0]).keys()): # for each attribute
+        ws_EventUser.write(0, c, label) # make the column label in the first row
+        for r, event_user in enumerate(li_all_eu): # for each EventUser
+            ws_EventUser.write(r+1, c, getattr(event_user, label))
+
+    #  R1_Ix
+
+    for sheet, li in zip([ws_r1_ix, ws_r2_ix, ws_r3_ix], [li_r1_ix, li_r2_ix, li_r3_ix]): # for each round
+        for c, label in enumerate(dict(li[0]).keys()): # for each column / attribute of an interaction
+            sheet.write(0, c, label) # make the column label in the first row
+            for r, ix in enumerate(li):
+                sheet.write(r+1, c, getattr(ix, label))
+
+    #  R2_Ix
+
+
+    #  R3_Ix
+
 
     WB.save('example.xls')    
 
--- helpers.py
+++ helpers.py
@@ -34,7 +34,24 @@ import requests
 """                                FUNCTIONS                                """
 ###############################################################################
 
+def optimize_event_timing(m, w, mg, wg, num_sec_in_entire_event = 60 * 60):
+    """
+    Should each round be the same length of time?
+    """
 
+    sec_pregame = 60 * 10 # 600
+    sec_break_1 = 60 * 5  # 300
+    sec_break_2 = 60 * 5  # 300
+    sec_postgame = 60 * 5 # 300
+
+    sec_not_playing = sec_pregame + sec_break_1 + sec_break_2 + sec_postgame
+    sec_playing = num_sec_in_entire_event - sec_not_playing
+
+    r1_ix = (m + mg) ** 2
+    r2_ix = round(r1_ix / 4.0, 
+
+    return [sec_pregame, sec_break_1, sec_break_2, sec_postgame], \
+           [sec_r1, sec_r2, sec_r3]
 
 def filter_by_value(sequence, value):
     """
@@ -77,155 +94,6 @@ def filter_by_value(sequence, value):
 
 def mk_serial(eNum):
     return "{}{}".format("0"*(4 - len(str(eNum))), eNum)
-
-
-def register_with_Parse():
-    # Call "register(?,?,?)" to allow parse_rest / ParsePy to work. 
-    # register(APPLICATION_ID, REST_API_KEY, optional MASTER_KEY)
-    register(
-        "AKJFNWcTcG6MUeMt1DAsMxjwU62IJPJ8agbwJZDJ", 
-        "i8o0t6wg9GOTly0yaApY2c1zZNMvOqNhoWNuzHUS", 
-        master_key = "LbaxSV6u64DRUKxdtQphpYQ7kiaopBaRMY1PgCsv"
-    )
-    pass
-
-
-def batch_delete_from_Parse_all_objects_of_class(str_cls_name):
-    # Parse's default limit for query results is 100, and max is 1000,
-    # but each query has to be set that way. I should extend this
-    # function to query more than 1000.
-    #
-    # Fixed; now sleeps for (hi-lo)/30 seconds instead of 50/30 = 1.67 seconds.
-
-    # Query for all objects of the passed-in class name.
-    cls = Object.factory(str_cls_name)
-    all_objects_to_delete = list(cls.Query.all().limit(1000))
-    num_to_del = cls.Query.all().limit(1000).count()
-    num_deleted = 0
-
-    print("\n(deleting {} {} objects from Parse)".format(num_to_del, str_cls_name))
-
-    # for some reason, cls.Query.all() is only returning 100 results,
-    # so just keep querying until there's nothing left.
-    while True: 
-
-        # Return if there aren't any existing objects of that class
-        if num_deleted == num_to_del:
-            return
-
-        # Then, batch delete them.
-        batcher = ParseBatcher()
-
-        for x in range((num_to_del-1)/50 + 1):
-            lo = 50*x
-            hi = min(50*(x+1), num_to_del)
-            if lo == hi:
-                return
-            batcher.batch_delete(all_objects_to_delete[lo:hi])
-            num_deleted += hi - lo
-
-            sys.stdout.write("\r{} of {} old {}'s deleted ({}{})".format(
-                min(50 + (50*x), num_to_del),
-                num_to_del,
-                str_cls_name,
-                min(int(round((50*(x+1)*100.0)/num_to_del, 0)), 100),
-                "%"
-                ))
-            sys.stdout.flush() # must be done for it to work (why?)
-            time.sleep((hi-lo)/30.0) # explained above
-
-        sys.stdout.write("\n") # move cursor to the next line after we're done
-
-
-
-
-
-    pass
-
-
-def batch_upload_to_Parse(Parse_class_name, li_objects):
-    # avoids timeout when batch uploading / saving.
-
-    # Save multiple objects to Parse.
-    # Call batcher.batch_save on slices of the list no larger than 50.
-        # (Parse will timeout if 1800 requests are made in 60 seconds,
-        # hence the time.sleep((hi-lo)/30.0) every run of objects saved.)
-
-    batcher = ParseBatcher()
-
-    num_to_up = len(li_objects)
-
-    print("\n(uploading {} {} objects to Parse)".format(num_to_up, Parse_class_name))
-
-    for x in range((num_to_up-1)/50 + 1):
-        batcher = ParseBatcher()
-        lo = 50*x
-        hi = min(50 * (x+1), num_to_up)
-
-        batcher.batch_save(li_objects[lo:hi])
-
-        sys.stdout.write("\r{} of {} new {}'s uploaded ({}{})".format(
-            min(50 + (50*x), num_to_up), 
-            num_to_up,
-            Parse_class_name,
-            min(int(round((50*(x+1)*100.0)/num_to_up, 0)), 100),
-            "%"
-            ))
-        sys.stdout.flush() # must be done for it to work (why?)
-        time.sleep((hi-lo)/30.0) # explained above
-
-    sys.stdout.write("\n") # move the cursor to the next line after we're done
-
-    pass
-
-
-
-
-
-def batch_query(
-                Source, 
-                Cls, 
-                Filter = [None, None, None], 
-                Limit = 1000, 
-                OrderBy = None, 
-                Skip = 0
-                ):
-
-    if Source == "Parse":
-
-        # if Cls == User:
-        #     q = User.Query.all().limit(Limit)
-        # else:
-        #     q = Cls.Query.all().limit(Limit)
-        q_start = time.time()
-
-        if Filter == [None, None, None]:
-            q = Cls.Query.all().limit(Limit).skip(Skip)
-        else:
-            f = Filter[0]
-            op = Filter[1]
-            val = Filter[2]        
-            if f == "sex" and op == "=":
-                q = Cls.Query.all().filter(sex = val).limit(Limit).skip(Skip)
-            if f == "qNum" and op == "<=":
-                q = Cls.Query.all().filter(qNum__lte = val).limit(Limit).skip(Skip)
-
-        len_q = len(q)
-
-        q_time_taken = time.time() - q_start
-        q_time_taken = 0
-
-        print("\n(Sleeping for {} seconds after {} {} objects were batch queried from Parse)"\
-            .format(round(len_q/30.0 - q_time_taken, 2), len_q, Cls.__name__)
-            )
-        time.sleep(len_q/30.0 - q_time_taken)
-
-        if OrderBy != None:
-            q.order_by(OrderBy)
-
-        return q
-
-    pass
 
 
 
@@ -282,10 +150,6 @@ def create_SAC_database_in_Firebase(create_SAC):
 
 
 def main():
-    register_with_Parse()
-    batch_delete_from_Parse_all_objects_of_class("Event")
-    batch_delete_from_Parse_all_objects_of_class("zE0000_User")
-   # batch_upload_to_Parse()
     pass
 
 
